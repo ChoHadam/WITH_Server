ffdc98bede43cae4c9aed33bae522f7dfa5fbe9c
MinJoon <mjun09@naver.com>
2019-12-24 14:42:31 +0900

    수정

--- with_server/routes/Auth/index.js
+++ with_server/routes/Auth/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
--- with_server/routes/index.js
+++ with_server/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
 
 /* GET home page. */
 router.use('/auth', require('./Auth'));
