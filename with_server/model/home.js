03681a63fd41bee35fa6124810b1eafdfad0063f
dgu-pmos <gudah1478@dongguk.edu>
2019-12-27 15:19:36 +0900

    home 수정

--- with_server/model/home.js
+++ with_server/model/home.js
@@ -1,25 +1,24 @@
 const pool = require('../module/db/pool');
 const table1 = 'User';
 const table2 = 'Region';
-const table3 = 'Board'
-module.exports = {
-    recommend: async(userIdx) => {
-        const record = await pool.queryParam_None(`SELECT record FROM ${table1} WHERE userIdx = '${userIdx}'`);
-        var region = String(record).substr(0,2);
-        var semi_region = String(record).substr(2,2);
-        var country = String(record).substr(4,2);
-        var result;
-        
-        // 모든 분류 출력
-        if(region == "00")
-        {
-            result =  await pool.queryParam_None(`SELECT * FROM ${table2} ORDER BY count LIMIT 6`)
-        }
-        else result = await pool.queryParam_None(`SELECT * FROM ${table2} WHERE regionCode LIKE '${region}____' ORDER BY count LIMIT 6`);
-    },
+const table3 = 'Board';
 
-    readMate: async(userIdx) => {
+module.exports = {
+    recommend: async (regionCode) => {
+        var region = String(regionCode).substr(0,2);
+        result =  await pool.queryParam_None(`SELECT regionName FROM ${table2} WHERE regionCode LIKE '%${region}%' ORDER BY count LIMIT 6`)
+        return result;
+    },
+    readMate: async (userIdx) => {
         const result = await pool.queryParam_None(`SELECT name, userImg FROM ${table1} WHERE userIdx = '${userIdx}'`);
         return result;    
+    },
+    readBoard: async (boardIdx) => {
+        const result = await pool.queryParam_None(`SELECT name, userImg, regionCode, title FROM ${table3} LEFT JOIN ${table1} ON Board.userIdx = User.userIdx WHERE boardIdx = '${boardIdx}'`);
+        return result;    
+    },
+    readAllRegion: async () => {
+        const result = await pool.queryParam_None(`SELECT * FROM Region`);
+        return result; 
     }
 };
--- with_server/routes/Home/index.js
+++ with_server/routes/Home/index.js
@@ -6,18 +6,15 @@ const statusCode = require('../../module/utils/statusCode');
 const Home = require('../../model/home');
 
 // 추천 동행지 보여주기
-router.get("/:userIdx", async (req, res) => {
-    const userIdx = req.params.userIdx;
+router.get("/recommendations/:regionCode", async (req, res) => {
+    const regionCode = req.params.regionCode;
     
-    if(!userIdx)
+    if(!regionCode)
     {
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.OUT_OF_VALUE));
         return;
     }
-
-    const result = await Home.recommend(userIdx);
-    
-    
+    const result = await Home.recommend(regionCode);
     if(result.length == 0)
     {
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.RECOMMEND_UPDATE_FAIL));
@@ -27,24 +24,61 @@ router.get("/:userIdx", async (req, res) => {
 });
 
 // 위드 메이트 보여주기
-router.get("/:userIdx/readMate", async (req, res) => {
+router.get("/mates/:userIdx/", async (req, res) => {
     const userIdx = req.params.userIdx;
-    
     if(!userIdx)
     {
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.OUT_OF_VALUE));
         return;
     }
 
-    const result = await Home.readMate(userIdx);
-    
+    user_arr = userIdx.split('+');
+    var result = [];
+    for ( var i in user_arr ) {
+        let user_list = await Home.readMate(user_arr[i]);
+        result.push(user_list);
+    }
     
     if(result.length == 0)
     {
         res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.MATE_READ_FAIL));
         return;
     }
-    
+
+    res.status(statusCode.OK).send(utils.successTrue(responseMessage.MATE_READ_SUCCESS, result));
+});
+
+router.get("/boards/:boardIdx", async (req, res) => {
+    const boardIdx = req.params.boardIdx;
+    if(!boardIdx)
+    {
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.OUT_OF_VALUE));
+        return;
+    }
+
+    board_arr = boardIdx.split('+');
+    var result = [];
+    for ( var i in board_arr ) {
+        let board_list = await Home.readBoard(board_arr[i]);
+        result.push(board_list);
+    }
+    if(result.length == 0)
+    {
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.MATE_READ_FAIL));
+        return;
+    }
+
+    res.status(statusCode.OK).send(utils.successTrue(responseMessage.MATE_READ_SUCCESS, result));
+});
+
+router.get("/regions", async (req, res) => {
+    const result = await Home.readAllRegion();
+
+    if(result.length == 0)
+    {
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.MATE_READ_FAIL));
+        return;
+    }
 
     res.status(statusCode.OK).send(utils.successTrue(responseMessage.MATE_READ_SUCCESS, result));
 });
