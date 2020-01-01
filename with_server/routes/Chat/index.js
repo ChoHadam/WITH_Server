9136d1b31253971a20407ca8e5bbe6e6d3765fcb
dgu-pmos <gudah1478@dongguk.edu>
2020-01-02 02:07:37 +0900

    채팅 기능 추가 및 수정

--- with_server/routes/Chat/index.js
+++ with_server/routes/Chat/index.js
@@ -14,18 +14,18 @@ const Chat = require('../../model/chat');
 let iconv = require('iconv-lite');
 
 router.post('/', authUtil.validToken, async (req, res) => {
-    const {receiverIdx, boardIdx} = req.body;
+    const {receiverIdx, boardIdx, roomId} = req.body;
     const senderIdx = req.decoded.userIdx;
-    if(!senderIdx || !receiverIdx || !boardIdx)
+    if(!senderIdx || !receiverIdx || !boardIdx || !roomId)
     {
-        const missParameters = Object.entries({senderIdx, receiverIdx, boardIdx})
+        const missParameters = Object.entries({senderIdx, receiverIdx, boardIdx, roomId})
         .filter(it => it[1] == undefined).map(it => it[0]).join(',');
         res
         .status(statusCode.BAD_REQUEST)
         .send(utils.successFalse(responseMessage.X_NULL_VALUE(missParameters)));
         return;
     }
-    const json = {senderIdx, receiverIdx, boardIdx};
+    const json = {senderIdx, receiverIdx, boardIdx, roomId};
     const check = await Chat.checkRoom(json);
     if(!check.length==0)
     {
@@ -86,6 +86,30 @@ router.get('/', authUtil.validToken, async (req, res) => {
         result[i].startDate = moment(result[i].startDate, 'YYYY-MM-DD').format('YY.MM.DD');
         result[i].endDate = moment(result[i].endDate, 'YYYY-MM-DD').format('YY.MM.DD');
         result[i].withDate = moment(result[i].withDate, 'YYYY-MM-DD').format('YY.MM.DD');
+    }
+    res.status(statusCode.OK).send(utils.successTrue(responseMessage.CHAT_READ_ALL_SUCCESS, result));
+});
+
+router.get('/check', authUtil.validToken, async (req, res) => {
+    const userIdx = req.decoded.userIdx;
+    if(!userIdx)
+    {
+        const missParameters = Object.entries({myIdx})
+        .filter(it => it[1] == undefined).map(it => it[0]).join(',');
+        res
+        .status(statusCode.BAD_REQUEST)
+        .send(utils.successFalse(responseMessage.X_NULL_VALUE(missParameters)));
+        return;
+    }
+    let result = await Chat.check(userIdx);
+    if(result.length == 0)
+    {
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(responseMessage.CHAT_READ_ALL_FAIL));
+        return;
+    }
+    for(var i in result){
+        result_img = await Chat.img(result[i].regionCode)
+        result[i].regionImgE = result_img[0].regionImgE;
     }
     res.status(statusCode.OK).send(utils.successTrue(responseMessage.CHAT_READ_ALL_SUCCESS, result));
 });
