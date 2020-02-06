d12a7eaf239fac3430a2092ae7c3abb8ed1133f0
MinJoon <mjun09@naver.com>
2020-02-06 17:01:52 +0900

    base url 출력 수정

--- with_server/routes/index.js
+++ with_server/routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router({mergeParams: true});
 
-/* GET home page. */
 router.use('/auth', require('./Auth'));
 router.use('/board', require('./Board'));
 router.use('/mypage', require('./Mypage'));
 router.use('/home', require('./Home'));
 router.use('/chat', require('./Chat'));
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.send('Welcome to the sweet WITH house ＼º▽º/')
+})
 
 module.exports = router;
