6a12f8c791884d2d4f1ac3bc16b392db1ae57593
MinJoon <mjun09@naver.com>
2020-02-14 01:00:00 +0900

    updates: 둘러보기 기능 구현

--- with_server/routes/Auth/index.js
+++ with_server/routes/Auth/index.js
@@ -3,5 +3,6 @@ const router = express.Router({mergeParams: true});
 
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
+router.use('/lookaround', require('./lookaround'));
 
 module.exports = router;
