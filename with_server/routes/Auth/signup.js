9a94a7c6ef64798e82e904bf5b7c5239f2ad83ba
jooo1431 <joo9610@gmail.com>
2020-02-19 15:19:50 +0900

    master pull waiting

--- with_server/routes/Auth/signup.js
+++ with_server/routes/Auth/signup.js
@@ -7,7 +7,7 @@ const utils = require('../../module/utils/utils');
 const upload = require('../../config/multer');
 const User = require('../../model/user');
 
-router.post('/',upload.single('img'), async (req, res) => {    
+router.post('/',upload.single('userImg'), async (req, res) => {    
     //필수항목 안채웠으면 오류메세지 전송
     const {userId, password, name, birth, gender} = req.body;
 
@@ -15,23 +15,21 @@ router.post('/',upload.single('img'), async (req, res) => {
         const missParameters = Object.entries({userId, password, name, birth, gender})
         .filter(it => it[1] == undefined).map(it => it[0]).join(',');
         res.status(statusCode.BAD_REQUEST)
-        .send(utils.successFalse(responseMessage.X_NULL_VALUE(missParameters)));
+        .send(utils.successFalse(statusCode.BAD_REQUEST, responseMessage.X_NULL_VALUE(missParameters)));
         return;
     }
-    //빈 항목이 없다면 그 아이디 중복되는지 확인
-    console.log("들어와라");
+    //빈 항목이 없다면 그 아이디 중복되는지 확인    
     checkDup = await User.checkUser(userId);
     if(!checkDup.length==0)
     {
         res
         .status(statusCode.BAD_REQUEST)
-        .send(utils.successFalse(responseMessage.ALREADY_ID));
+        .send(utils.successFalse(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID));
         return;
     }
     //중복되는 아이디가 없다면 회원가입 시작 
     
-    var userImg = req.file.location; //s3에 저장된 이미지 url
-    console.log(userImg);    
+    var userImg = req.file.location; //s3에 저장된 이미지 url      
     
     const buf = await crypto.randomBytes(32); //64비트의 salt값 생성
     const salt = buf.toString('hex'); //비트를 문자열로 바꿈
@@ -43,12 +41,13 @@ router.post('/',upload.single('img'), async (req, res) => {
     if(!result){
         res
         .status(statusCode.INTERNAL_SERVER_ERROR)
-        .send(utils.successFalse(responseMessage.INTERNAL_SERVER_ERROR));
+        .send(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
         return;
     }
+    console.log(result);
     res
     .status(statusCode.OK)
-    .send(utils.successTrue(responseMessage.SIGN_UP_SUCCESS)); 
+    .send(utils.successTrue(statusCode.OK, responseMessage.SIGN_UP_SUCCESS, result)); 
 });
 
 module.exports = router;
