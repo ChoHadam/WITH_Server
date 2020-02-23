3a66817253980a3a262625f691dac71d6a2bf883
hadam <hadam97@naver.co>
2020-02-23 20:11:23 +0900

    회원가입 관심사 코드 수정

--- with_server/model/user.js
+++ with_server/model/user.js
@@ -5,7 +5,13 @@ const user = {
     signup : async (json) => {
         //아이디, 비번, 이름, 생일, 성별, 프로필사진
         const fields = 'userId, password, salt, name, birth, gender, userImg, interest1, interest2, interest3';
-        const questions = `"${json.userId}","${json.finalPw}","${json.salt}","${json.name}","${json.birth}","${json.gender}","${json.userImg}", "${json.interest1}", "${json.interest2}", "${json.interest3}" `;        
+        var questions = `"${json.userId}","${json.finalPw}","${json.salt}","${json.name}","${json.birth}","${json.gender}","${json.userImg}" `;
+        if (json.interest1) questions +=`,"${json.interest1}"`
+        else questions += `, null `
+        if (json.interest2) questions +=`,"${json.interest2}"`
+        else questions += `, null `
+        if (json.interest3) questions +=`,"${json.interest3}"`
+        else questions += `, null`    
         const result = await pool.queryParam_None(`INSERT INTO ${table}(${fields})VALUES(${questions})`)
         return result;
     },
