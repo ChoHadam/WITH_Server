cdd9143b14c70c8cf06f1e00a2fe42521956168d
MinJoon <mjun09@naver.com>
2020-03-08 00:48:36 +0900

    fixed

--- with_server/routes/Mypage/index.js
+++ with_server/routes/Mypage/index.js
@@ -150,7 +150,7 @@ router.put("/changePw", authUtil.validToken, async(req, res) => {
             const buf = await crypto.randomBytes(32); //64비트의 salt값 생성
             const salt = buf.toString('hex'); //비트를 문자열로 바꿈
             const hashedPw = await crypto.pbkdf2(newPw.toString(),salt,1000,32,'SHA512'); //버퍼 형태로 리턴해주기 때문에 base64 방식으로 문자열
-            const finalPw = hashedPw.toString('hex'); 
+            const finalPw = hashedPw.toString('hex');
             var json = {userIdx, finalPw, salt};
 
             result = Mypage.changePw(json);
