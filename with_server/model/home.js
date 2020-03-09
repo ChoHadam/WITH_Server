d8a427bc9a82524ec62752b28ab1092368ea3b95
MinJoon <mjun09@naver.com>
2020-03-09 22:38:43 +0900

    fixed: 국가리스트 출력 치명적 오류 수정

--- with_server/model/home.js
+++ with_server/model/home.js
@@ -4,101 +4,102 @@ const table2 = 'Region';
 const table3 = 'Board';
 const table4 = 'Home_img';
 
-var continent = new Object();
-continent.europe = new Object();
-continent.asia = new Object();
-continent.north_america = new Object();
-continent.south_america = new Object();
-continent.oceania = new Object();
-continent.africa = new Object();
-continent.korea = new Object();
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-continent.europe.all = new Object();
-continent.europe.east = new Object();
-continent.europe.west = new Object();
-continent.europe.north = new Object();
-continent.europe.south = null;
-
-continent.asia.all = new Object();
-continent.asia.east = new Object();
-continent.asia.west = new Object();
-continent.asia.north = new Object();
-continent.asia.south = new Object();
-
-continent.north_america.all = new Object();
-continent.north_america.east = null;
-continent.north_america.west = null;
-continent.north_america.north = null;
-continent.north_america.south = null;
-
-continent.south_america.all = new Object();
-continent.south_america.east = null;
-continent.south_america.west = null;
-continent.south_america.north = null;
-continent.south_america.south = null;
-
-continent.oceania.all = new Object();
-continent.oceania.east = null;
-continent.oceania.west = null;
-continent.oceania.north = null;
-continent.oceania.south = null;
-
-continent.africa.all = new Object();
-continent.africa.east = new Object();
-continent.africa.west = null;
-continent.africa.north = new Object();
-continent.africa.south = new Object();
-
-continent.korea.all = new Object();
-continent.korea.east = null;
-continent.korea.west = null;
-continent.korea.north = null;
-continent.korea.south = null;
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-continent.europe.all.name = "유럽 전체";
-continent.europe.all.country = new Array();
-continent.europe.east.name = "동유럽";
-continent.europe.east.country = new Array();
-continent.europe.west.name = "서유럽";
-continent.europe.west.country = new Array();
-continent.europe.north.name = "북유럽";
-continent.europe.north.country = new Array();
-
-continent.asia.all.name = "아시아 전체";
-continent.asia.all.country = new Array();
-continent.asia.east.name = "동남아시아";
-continent.asia.east.country = new Array();
-continent.asia.west.name = "서남아시아";
-continent.asia.west.country = new Array();
-continent.asia.north.name = "동북아시아";
-continent.asia.north.country = new Array();
-continent.asia.south.name = "남부아시아";
-continent.asia.south.country = new Array();
-
-continent.north_america.all.name = "북아메리카 전체";
-continent.north_america.all.country = new Array();
-
-continent.south_america.all.name = "남아메리카 전체";
-continent.south_america.all.country = new Array();
-
-continent.oceania.all.name = "오세아니아 전체";
-continent.oceania.all.country = new Array();
-
-continent.africa.all.name = "아프리카 전체";
-continent.africa.all.country = new Array();
-continent.africa.east.name = "동아프리카";
-continent.africa.east.country = new Array();
-continent.africa.north.name = "북아프리카";
-continent.africa.north.country = new Array();
-continent.africa.south.name = "남아프리카";
-continent.africa.south.country = new Array();
-
-continent.korea.all.name = "국내 전체";
-continent.korea.all.country = new Array();
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
 module.exports = {
     readRegion: async () => {
+        var continent = new Object();
+        continent.europe = new Object();
+        continent.asia = new Object();
+        continent.north_america = new Object();
+        continent.south_america = new Object();
+        continent.oceania = new Object();
+        continent.africa = new Object();
+        continent.korea = new Object();
+        /* - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+        continent.europe.all = new Object();
+        continent.europe.east = new Object();
+        continent.europe.west = new Object();
+        continent.europe.north = new Object();
+        continent.europe.south = null;
+        
+        continent.asia.all = new Object();
+        continent.asia.east = new Object();
+        continent.asia.west = new Object();
+        continent.asia.north = new Object();
+        continent.asia.south = new Object();
+        
+        continent.north_america.all = new Object();
+        continent.north_america.east = null;
+        continent.north_america.west = null;
+        continent.north_america.north = null;
+        continent.north_america.south = null;
+        
+        continent.south_america.all = new Object();
+        continent.south_america.east = null;
+        continent.south_america.west = null;
+        continent.south_america.north = null;
+        continent.south_america.south = null;
+        
+        continent.oceania.all = new Object();
+        continent.oceania.east = null;
+        continent.oceania.west = null;
+        continent.oceania.north = null;
+        continent.oceania.south = null;
+        
+        continent.africa.all = new Object();
+        continent.africa.east = new Object();
+        continent.africa.west = null;
+        continent.africa.north = new Object();
+        continent.africa.south = new Object();
+        
+        continent.korea.all = new Object();
+        continent.korea.east = null;
+        continent.korea.west = null;
+        continent.korea.north = null;
+        continent.korea.south = null;
+        /* - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+        continent.europe.all.name = "유럽 전체";
+        continent.europe.all.country = new Array();
+        continent.europe.east.name = "동유럽";
+        continent.europe.east.country = new Array();
+        continent.europe.west.name = "서유럽";
+        continent.europe.west.country = new Array();
+        continent.europe.north.name = "북유럽";
+        continent.europe.north.country = new Array();
+        
+        continent.asia.all.name = "아시아 전체";
+        continent.asia.all.country = new Array();
+        continent.asia.east.name = "동남아시아";
+        continent.asia.east.country = new Array();
+        continent.asia.west.name = "서남아시아";
+        continent.asia.west.country = new Array();
+        continent.asia.north.name = "동북아시아";
+        continent.asia.north.country = new Array();
+        continent.asia.south.name = "남부아시아";
+        continent.asia.south.country = new Array();
+        
+        continent.north_america.all.name = "북아메리카 전체";
+        continent.north_america.all.country = new Array();
+        
+        continent.south_america.all.name = "남아메리카 전체";
+        continent.south_america.all.country = new Array();
+        
+        continent.oceania.all.name = "오세아니아 전체";
+        continent.oceania.all.country = new Array();
+        
+        continent.africa.all.name = "아프리카 전체";
+        continent.africa.all.country = new Array();
+        continent.africa.east.name = "동아프리카";
+        continent.africa.east.country = new Array();
+        continent.africa.north.name = "북아프리카";
+        continent.africa.north.country = new Array();
+        continent.africa.south.name = "남아프리카";
+        continent.africa.south.country = new Array();
+        
+        continent.korea.all.name = "국내 전체";
+        continent.korea.all.country = new Array();
+        /* - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+
+
         var jsonData = JSON.stringify(continent);
         const result = await pool.queryParam_None(`SELECT regionName, regionCode FROM ${table2}`);
         
